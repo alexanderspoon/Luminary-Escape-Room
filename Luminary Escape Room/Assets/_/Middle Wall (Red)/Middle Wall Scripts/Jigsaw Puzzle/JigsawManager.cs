51ce922d0ea9aceda86932137b90bdee6d150984
Jacob Palin <jacobcpalin@gmail.com>
2024-04-03 16:50:12 -0600

    items added to main scenes

--- Luminary Escape Room/Assets/_/Middle Wall (Red)/Middle Wall Scripts/Jigsaw Puzzle/JigsawManager.cs
+++ Luminary Escape Room/Assets/_/Middle Wall (Red)/Middle Wall Scripts/Jigsaw Puzzle/JigsawManager.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class JigsawManager : MonoBehaviour
 {
-    public static int slotsFilled; 
+    public static int slotsFilled;
+    [SerializeField] private GameObject frame;
+    [SerializeField] private GameObject pieces;
+    [SerializeField] private GameObject scroll;
+
+    private void Awake()
+    {
+        frame.SetActive(true);
+        pieces.SetActive(true);
+        scroll.SetActive(false);
+    }
 
     void Update()
     {
@@ -13,6 +23,9 @@ public class JigsawManager : MonoBehaviour
         {
             // Fill in progression code here
             Debug.Log("The Puzzle is Finished!");
+            frame.SetActive(false);
+            pieces.SetActive(false);
+            scroll.SetActive(true);
         }
     }
 }
