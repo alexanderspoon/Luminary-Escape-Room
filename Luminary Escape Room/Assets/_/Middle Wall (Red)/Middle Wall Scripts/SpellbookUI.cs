63eade8aaf8a4118ba186d51a33659a9787ad49f
Jacob Palin <jacobcpalin@gmail.com>
2024-04-14 11:42:34 -0600

    spellbook UI

---
+++ Luminary Escape Room/Assets/_/Middle Wall (Red)/Middle Wall Scripts/SpellbookUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellbookUI : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject spellbookCanvas;
+
+    private void Awake()
+    {
+        spellbookCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            //add object to inventory if it has the ItemPickable script component and it will be destroyed
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if(hitInfo.collider.GetComponent<SpellbookUI>()  != null)
+                {
+                    spellbookCanvas.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void DismissButton()
+    {
+        spellbookCanvas.SetActive(false);
+    }
+}
